b2d6a0dcba32000e250cd6c1c31c211295dad615
SL <534336+eleventy7@users.noreply.github.com>
2021-12-22 15:54:32 -0500

    Fix copyrights across project

--- eider-internals/src/main/java/io/eider/internals/PreprocessedEiderMessage.java
+++ eider-internals/src/main/java/io/eider/internals/PreprocessedEiderMessage.java
@@ -1,7 +1,5 @@
-package io.eider.internals;
-
 /*
- * Copyright 2019-2021 Shaun Laurens.
+ * Copyright ©2019-2022 Shaun Laurens
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,9 +10,11 @@ package io.eider.internals;
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *
+ * See the License for the specific language governing permissions and limitations under the License.
  */
+
+package io.eider.internals;
 
 import java.util.List;
 
